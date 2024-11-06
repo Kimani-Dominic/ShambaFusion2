2afec7cfcf71d2095e17db8f169a928d75439ff1
Kimani-Dominic <muiruridominic001@gmail.com>
2024-11-06 16:53:31 +0300

    fix(app): Auth

--- src/hooks/useRole.jsx
+++ src/hooks/useRole.jsx
@@ -1,24 +1,30 @@
-
 import { createContext, useContext, useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 const RoleContext = createContext();
 
-export function useRole () {
+export function useRole() {
     return useContext(RoleContext);
-
 }
 
-export function RoleProvider({children}) {
-    const [role, setRole] = useState('farmer');
+export function RoleProvider({ children }) {
+    const [role, setRole] = useState(null);
+    const navigate = useNavigate();
 
     const changeRole = (newRole) => {
         setRole(newRole);
-    }
+        if (newRole === 'seller') {
+            navigate('/seller-dashboard');
+        } else if (newRole === 'buyer') {
+            navigate('/buyer-dashboard');
+        } else if (newRole === 'vendor') {
+            navigate('/vendor-dashboard');
+        }
+    };
 
-    return(
-        <RoleContext.Provider value={{role, changeRole}} >
+    return (
+        <RoleContext.Provider value={{ role, changeRole }}>
             {children}
         </RoleContext.Provider>
-    )
+    );
 }
-
